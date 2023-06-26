16fd57c250aa1e5d4b28b7b83fdcae5320a452d1
Jose Miguel <7010@holbertonstudents.com>
2023-06-25 20:39:53 -0700

    task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	putchar(num + '0');
+
+	while (num++ < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	putchar(num + '0');
+	}
+
+	putchar('\n');
+	return (0);
+}
