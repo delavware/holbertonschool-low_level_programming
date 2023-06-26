9315fb3b9ba41049529c13ef615726480ad595f6
Jose Miguel <7010@holbertonstudents.com>
2023-06-25 20:13:50 -0700

    task 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,14 +6,16 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) {
-    int num;
+int main(void)
+{
+	int num;
 
-    for (num = 0; num <= 9; num++) {
-        putchar('0' + num) ;
-    }
+	for (num = 0; num <= 9; num++)
+	{
+	putchar('0' + num);
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
